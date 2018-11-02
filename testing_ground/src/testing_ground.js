601801e1842044f3cd39c8823d5ea15795309e74
Dale Euinton <dale.euinton@gmail.com>
2018-11-02 19:37:56 +1300

    Got hovering going and different selection and some more generally cleaner code.

--- testing_ground/src/testing_ground.js
+++ testing_ground/src/testing_ground.js
@@ -17,10 +17,18 @@ var Rectangle = /** @class */ (function () {
         this.y = y || 0;
         this.width = width || 0;
         this.height = height || 0;
-        this.fill = fill || '#AAAAAA';
+        this.fill = fill || "#32FAFA";
+        this.selectionColor = "#105151";
+        this.beingHovered = false;
+        this.hoverColor = "#2EE6E6";
     }
     Rectangle.prototype.draw = function (context) {
-        context.fillStyle = this.fill;
+        if (this.beingHovered) {
+            context.fillStyle = this.hoverColor;
+        }
+        else {
+            context.fillStyle = this.fill;
+        }
         context.fillRect(this.x, this.y, this.width, this.height);
     };
     Rectangle.prototype.contains = function (x, y) {
@@ -51,70 +59,34 @@ var CanvasState = /** @class */ (function () {
         this.htmlLeft = this.html.offsetLeft;
         this.valid = false;
         this.shapes = [];
+        this.nShapes = 0;
         this.dragging = false;
         this.selection = null;
         this.dragOffsetX = 0;
         this.dragOffsetY = 0;
         this.mState = this;
-        this.selectionColor = '#CC0000'; // This should be incldued in the shapes class as a different one for each shape
-        this.selectionWidth = 10;
+        this.selectionWidth = 2;
         this.timeInterval = 30;
-        // // What is e?
-        // canvas.addEventListener('selectstart', function(e) {e.preventDefault(); return false})
-        // // Test for my own purposes ,can you use speech marks not just apostrophes
-        // canvas.addEventListener('mousedown', function(e) {
-        // 	var mouse:any = this.getMouse(e)
-        // 	var mouseX:number = mouse.x
-        // 	var mouseY:number = mouse.y
-        // 	var shapes:Rectangle[] = this.mState.shapes
-        // 	var nShapes:number = shapes.length
-        // 	// for (var i = nShapes - 1; i >= 0; i--) { // his implementation if it doesn't work
-        // 	for (var i = 0; i < nShapes ; i++) {
-        // 		if (shapes[i].contains(mouseX, mouseY)) {
-        // 			var mSelection:Rectangle = shapes[i]							// SHould this be a let. Definitely test this tomorrow!
-        // 			this.mState.dragOffsetX = mouseX - mSelection.x
-        // 			this.mState.dragOffsetY = mouseY - mSelection.y
-        // 			this.mState.dragging = true
-        // 			this.mState.selection = mSelection
-        // 			this.mState.valid = false
-        // 			return
-        // 		}
-        // 	}
-        // 	if (this.mState.selection) {
-        // 		this.mState.selection = null
-        // 		this.mState.valid = false
-        // 	}
-        // }, true) 												// Why is there a true there, look up api
-        // canvas.addEventListener('mousemove', function(e) {
-        // 	if (this.dragging) {
-        // 		var mouse:any = this.mState.getMouse(e)
-        // 		this.mState.selection.x = mouse.x - this.mState.dragOffsetX
-        // 		this.mState.selection.y = mouse.y - this.mState.dragOffsetY
-        // 		this.mState.valid = false
-        // 	}
-        // }, true)
-        // canvas.addEventListener('dblclick', function(e) {
-        // 	var mouse:any = this.mState.getMouse()
-        // 	this.mState.addShape(new Rectangle(mouse.x - 10, mouse.y - 10, 20, 20, 'rgba(0, 255, 0, 0.6)'))			// these hardcoded things are definitely worth changing
-        // }, true)
     }
     CanvasState.prototype.init = function () {
         var mCanvas = this.mState;
         this.canvas.addEventListener('selectstart', function (e) { e.preventDefault(); return false; });
-        this.canvas.addEventListener('mousedown', function (e, Canvas) {
+        this.canvas.addEventListener("mousedown", function (e, Canvas) {
+            // console.log("MouseDown")
             var mouse = mCanvas.getMouse(e);
             var mouseX = mouse.x;
             var mouseY = mouse.y;
-            var shapes = mCanvas.shapes;
-            var nShapes = shapes.length;
-            // for (var i = nShapes - 1; i >= 0; i--) { // his implementation if it doesn't work
-            for (var i = 0; i < nShapes; i++) {
-                if (shapes[i].contains(mouseX, mouseY)) {
-                    var mSelection = shapes[i]; // SHould this be a let. Definitely test this tomorrow!
-                    mCanvas.dragOffsetX = mouseX - mSelection.x;
-                    mCanvas.dragOffsetY = mouseY - mSelection.y;
+            // let shapes:Rectangle[] = mCanvas.shapes
+            // let nShapes:number = shapes.length
+            // for (var i = mCanvas.nShapes - 1; i >= 0; i--) { 
+            for (var i = 0; i < mCanvas.nShapes; i++) {
+                if (mCanvas.shapes[i].contains(mouseX, mouseY)) {
+                    mCanvas.dragOffsetX = mouseX - mCanvas.shapes[i].x;
+                    mCanvas.dragOffsetY = mouseY - mCanvas.shapes[i].y;
+                    mCanvas.shapes[i].beingHovered = false;
                     mCanvas.dragging = true;
-                    mCanvas.selection = mSelection;
+                    mCanvas.selection = mCanvas.shapes[i];
+                    mCanvas.moveToFrontOfShapes(mCanvas.shapes, mCanvas.shapes[i], i);
                     mCanvas.valid = false;
                     return;
                 }
@@ -124,33 +96,74 @@ var CanvasState = /** @class */ (function () {
                 mCanvas.valid = false;
             }
         }, true);
+        this.canvas.addEventListener("mouseup", function (e) {
+            // console.log("MouseUp")
+            if (mCanvas.selection) {
+                // mCanvas.selection = null
+                mCanvas.dragging = false;
+                mCanvas.valid = false;
+            }
+        }, true);
+        this.canvas.addEventListener('mousemove', function (e) {
+            var mouse = mCanvas.getMouse(e);
+            if (mCanvas.dragging) {
+                mCanvas.selection.x = mouse.x - mCanvas.dragOffsetX;
+                mCanvas.selection.y = mouse.y - mCanvas.dragOffsetY;
+                mCanvas.valid = false;
+                return;
+            }
+            if (mCanvas.selection === null) {
+                for (var i = 0; i < mCanvas.nShapes; i++) {
+                    if (mCanvas.shapes[i].beingHovered) {
+                        mCanvas.shapes[i].beingHovered = false;
+                        mCanvas.valid = false;
+                    }
+                    if (mCanvas.shapes[i].contains(mouse.x, mouse.y)) {
+                        mCanvas.shapes[i].beingHovered = true;
+                        mCanvas.valid = false;
+                        // console.log("hovered")
+                        return;
+                    }
+                }
+            }
+            // console.log("Still reach here")
+        }, true);
+        this.canvas.addEventListener('dblclick', function (e) {
+            var mouse = mCanvas.getMouse(e);
+            mCanvas.addShape(new Rectangle(mouse.x - 10, mouse.y - 10, 20, 20, 'rgba(0, 255, 0, 0.6)')); // these hardcoded things are definitely worth changing
+        }, true);
+    };
+    CanvasState.prototype.moveToFrontOfShapes = function (shapes, s, index) {
+        if (index > 0) {
+            for (var i = index; i > 0; i--) {
+                shapes[i] = shapes[i - 1];
+            }
+            shapes[0] = s;
+        }
     };
     CanvasState.prototype.addShape = function (shape) {
         this.shapes.push(shape);
         this.valid = false;
+        this.nShapes++;
     };
     CanvasState.prototype.clear = function () {
         this.context.clearRect(0, 0, this.width, this.height);
     };
     CanvasState.prototype.draw = function () {
         if (!this.valid) {
-            var context = this.context; // again whats going on here with the var && why are we getting var?
-            var shapes = this.shapes;
             this.clear();
-            var nShapes = shapes.length; // This could easily be a class variable that is updated with each add or delete shape
-            for (var i = 0; i < nShapes; i++) {
-                var shape = shapes[i];
-                if (shape.x > this.width || shape.y > this.height ||
-                    shape.x + shape.width < 0 || shape.y + shape.height < 0) {
+            var nShapes = this.shapes.length;
+            for (var i = nShapes - 1; i >= 0; i--) {
+                if (this.shapes[i].x > this.width || this.shapes[i].y > this.height ||
+                    this.shapes[i].x + this.shapes[i].width < 0 || this.shapes[i].y + this.shapes[i].height < 0) {
                     continue;
                 }
-                shapes[i].draw(context);
+                this.shapes[i].draw(this.context);
             }
             if (this.selection != null) {
-                context.strokeStyle = this.selectionColor;
-                context.lineWidth = this.selectionWidth;
-                var mSel = this.selection; // can I get rid of stuff like this in favor using selection
-                context.strokeRect(mSel.x, mSel.y, mSel.width, mSel.height);
+                this.context.strokeStyle = this.selection.selectionColor;
+                this.context.lineWidth = this.selectionWidth;
+                this.context.strokeRect(this.selection.x, this.selection.y, this.selection.width, this.selection.height);
             }
             this.valid = true;
         }
@@ -177,7 +190,8 @@ var CanvasState = /** @class */ (function () {
 }());
 var s = new CanvasState(document.getElementById('canvas'));
 s.init();
-s.addShape(new Rectangle(40, 40, 50, 50, 'lightskyblue'));
+// s.addShape(new Rectangle(40, 40, 50, 50, 'lightskyblue'))
+s.addShape(new Rectangle(40, 40, 50, 50));
 setInterval(function () { s.mState.draw(); }, s.timeInterval); // would love to know what this does
 // init()
 // wtf is jQuery
